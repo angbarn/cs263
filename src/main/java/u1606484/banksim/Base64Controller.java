7152e44742bafba9817d9685c803bccd774e46bc
angbarn <a.barnes.3@warwick.ac.uk>
2018-02-26 13:22:06 +0000

    Extracted Base64 encoding/decoding into new class

---
+++ src/main/java/u1606484/banksim/Base64Controller.java
@@ -0,0 +1,25 @@
+package u1606484.banksim;
+
+import java.util.Base64;
+
+public class Base64Controller {
+	public static String toBase64(String data) {
+		return toBase64(data.getBytes());
+	}
+
+	public static String toBase64(byte[] data) {
+		return new String(Base64.getEncoder().encode(data));
+	}
+
+	public static byte[] fromBase64(byte[] data) {
+		return Base64.getDecoder().decode(data);
+	}
+
+	public static byte[] fromBase64(String data) {
+		return fromBase64(data.getBytes());
+	}
+
+	public static String bytesToRawString(byte[] data) {
+		return new String(data);
+	}
+}
