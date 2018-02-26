d7521b3df0e76bedb014a52ada2203f460ad4260
angbarn <a.barnes.3@warwick.ac.uk>
2018-02-26 10:41:29 +0000

    New tests for AesEncryption

---
+++ src/main/java/u1606484/banksim/AesEncryption.java
@@ -0,0 +1,121 @@
+package u1606484.banksim;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * This class provides the functionality to encrypt or decrypt files. <p>In
+ * order to encrypt a file, the {@link AesEncryption#swap} method is called with
+ * a parameter of {@code Cipher.ENCRYPT_MODE}. Similarly, to decrypt a file, the
+ * same method is called, but with a parameter of {@code Cipher.DECRYPT_MODE}.
+ * <p>No other modes of operation are available. <p>This is a very high level
+ * class, so no padding of data / other processing should be necessary.
+ */
+public class AesEncryption {
+
+	/**
+	 * Algorithm to use for encryption/decryption
+	 */
+	private static final String ALGO = "AES";
+
+	/**
+	 * Reads a file's binary data as an array of bytes
+	 *
+	 * @param fileName The path to the file to read
+	 * @return Array of bytes for binary data in file
+	 */
+	private static byte[] readFile(String fileName) {
+		try {
+			Path path = Paths.get(fileName);
+			return Files.readAllBytes(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("IOE");
+		}
+	}
+
+	/**
+	 * Writes an array of bytes to a file as binary data
+	 *
+	 * @param data The binary data to write to file
+	 * @param fileName The path to save the file
+	 */
+	private static void writeFile(byte[] data, String fileName) {
+		try (FileOutputStream fos = new FileOutputStream(fileName)) {
+			fos.write(data);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("IOE");
+		}
+	}
+
+	/**
+	 * Encrypt or decrypt a file, depending on the mode. <p>A vast majority of
+	 * the work encrypting and decrypting a file is the same, so this function
+	 * combines both with a "mode" variable to toggle between them.
+	 *
+	 * @param fileName The path to the target file
+	 * @param password The password to use for encryption/decryption
+	 * @param mode Whether encrypting or decrypting
+	 * @param extension The extension to add to the output file so that it
+	 * isn't
+	 * overwritten.
+	 */
+	public static void swapFile(String fileName, String password, int mode,
+			String extension) throws IllegalBlockSizeException,
+			NoSuchAlgorithmException, NoSuchPaddingException,
+			InvalidKeyException, BadPaddingException {
+		byte[] data = readFile(fileName);
+		byte[] newData = swap(data, password, mode);
+		writeFile(newData, fileName + extension);
+	}
+
+	/**
+	 * Encrypts or decrypts an array of bytes using a specified password <p>A
+	 * vast majority of the work encrypting or decrypting data is the same, so
+	 * this method combines both with a "mode" argument to toggle.
+	 *
+	 * @param data The data to process
+	 * @param password The password to encrypt/decrypt with
+	 * @param opmode Whether to encrypt or decrypt
+	 */
+	private static byte[] swap(byte[] data, String password, int opmode)
+			throws NoSuchAlgorithmException, IllegalBlockSizeException,
+			NoSuchPaddingException, InvalidKeyException, BadPaddingException {
+		if (opmode != Cipher.DECRYPT_MODE && opmode != Cipher.ENCRYPT_MODE) {
+			throw new IllegalArgumentException("Must encrypt or decrypt");
+		}
+
+		Key key = new SecretKeySpec(password.getBytes(), ALGO);
+
+		Cipher c = Cipher.getInstance(ALGO);
+		c.init(opmode, key);
+
+		// Do the business
+		return c.doFinal(data);
+	}
+
+	public static void main(String[] arguments) {
+		SecureRandom random = new SecureRandom();
+		for (int i = 0; i < 10; i++) {
+			byte[] nonce = new byte[20];
+			random.nextBytes(nonce);
+			byte[] encodedNonce = Base64.getEncoder().encode(nonce);
+			String readableNonce = new String(encodedNonce);
+			System.out.println(readableNonce + " " + readableNonce.length());
+		}
+	}
+}
