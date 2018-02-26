45a92934fd67efd53c1bf219a96582ce886d7221
angbarn <a.barnes.3@warwick.ac.uk>
2018-02-26 18:36:19 +0000

    Skeleton log in

---
+++ src/main/java/u1606484/banksim/LoginSystem.java
@@ -0,0 +1,83 @@
+package u1606484.banksim;
+
+import java.util.Base64;
+
+public class LoginSystem {
+	private static final int OTAC_PRECISION = 60 * 5;
+
+	private static byte[] getArbitraryBytes(int len) {
+		byte[] ret = new byte[len];
+		for (int i = 0; i < ret.length; i++) {
+			ret[i] = 0;
+		}
+		return ret;
+	}
+
+	// TODO
+	private static byte[] getPassword(int userId) {
+//		return getArbitraryBytes((int) (Math.random() * 10 + 5));
+		return "hello".getBytes();
+	}
+
+	private static boolean verifyPassword(int userId, String passwordAttempt) {
+		byte[] attemptHash = OtacGenerator.getHash(passwordAttempt);
+		byte[] trueHash = OtacGenerator.getHash(getPassword(userId));
+
+		boolean match = true;
+
+		if (trueHash.length == attemptHash.length) {
+			for (int i = 0; i < attemptHash.length; i++) {
+				if (trueHash[i] != attemptHash[i]) {
+					match = false;
+					break;
+				}
+			}
+		} else {
+			match = false;
+		}
+
+		return match;
+	}
+
+	private static String generateOtac(int userId) {
+		StringBuilder content = new StringBuilder();
+		byte[] nonce = getLoginNonce(userId);
+
+		content.append(userId);
+		content.append(Base64Controller.toBase64(nonce));
+
+		return OtacGenerator.generateOtac(OTAC_PRECISION, content.toString());
+	}
+
+	// TODO
+	private static byte[] getLoginNonce(int userId) {
+		int t = 20;
+		return getArbitraryBytes(t);
+	}
+
+	// TODO
+	private static void sendOtac(int userId) {
+		String email = userId + "@host.domain";
+		System.out.println(email + ": " + generateOtac(userId));
+	}
+
+	public static void main(String[] arguments) {
+		int targetUserId = 1;
+		String passwordAttempt = "hello";
+		String otacAttempt = generateOtac(targetUserId);
+
+		if (verifyPassword(targetUserId, passwordAttempt)) {
+			String otac = generateOtac(targetUserId);
+			sendOtac(targetUserId);
+
+			if (otac.equals(otacAttempt)) {
+				System.out.println("lottery");
+			} else {
+				System.out.println("Computer says no");
+			}
+		} else {
+			System.out.println("Computer says no");
+		}
+	}
+
+}
