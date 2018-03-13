2858e6cb74144841e835590299c2389b5038cc45
angbarn <a.barnes.3@warwick.ac.uk>
2018-03-13 00:13:24 +0000

    Rearranged IOtacGenerator and ITwoFactorService

--- src/main/java/u1606484/banksim/interfaces/IOtacGenerator.java
+++ src/main/java/u1606484/banksim/interfaces/IOtacGenerator.java
@@ -1,24 +1,10 @@
 package u1606484.banksim.interfaces;
 
-import java.util.stream.Stream;
-
 public interface IOtacGenerator {
 
     String generateOtac(byte[] secretKey, int stepOffset);
 
-    int getWindowSize();
-
     default String generateOtac(byte[] secretKey) {
         return generateOtac(secretKey, 0);
-    }
-
-    default boolean verifyOtac(String attempt, byte[] secretKey) {
-        return Stream
-                .iterate(0, x -> x + 1)
-                .limit(getWindowSize())
-                .map(i -> generateOtac(secretKey, i))
-                .map(attempt::equals)
-                .reduce((aBoolean, aBoolean2) -> aBoolean || aBoolean2)
-                .orElse(false);
     }
 }
--- src/main/java/u1606484/banksim/interfaces/ITwoFactorService.java
+++ src/main/java/u1606484/banksim/interfaces/ITwoFactorService.java
@@ -1,5 +1,7 @@
 package u1606484.banksim.interfaces;
 
+import java.util.stream.Stream;
+
 public interface ITwoFactorService {
 
     /**
@@ -10,4 +12,22 @@ public interface ITwoFactorService {
      * @param otac The OTAC to transmit
      */
     void sendTwoFactorCode(String contactAddress, String otac);
+
+    String generateOtac(byte[] secretKey, int offset);
+
+    default String generateOtac(byte[] secretKey) {
+        return generateOtac(secretKey, 0);
+    }
+
+    int getWindowSize();
+
+    default boolean verifyOtac(String attempt, byte[] secretKey) {
+        return Stream
+                .iterate(0, x -> x + 1)
+                .limit(getWindowSize())
+                .map(i -> generateOtac(secretKey, i))
+                .map(attempt::equals)
+                .reduce((aBoolean, aBoolean2) -> aBoolean || aBoolean2)
+                .orElse(false);
+    }
 }
