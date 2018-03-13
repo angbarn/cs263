4dfb78dba363addbc07928c1cbf6d3b445ae6afb
angbarn <a.barnes.3@warwick.ac.uk>
2018-03-13 09:29:44 +0000

    Added spring boot

--- src/main/java/u1606484/banksim/controllers/WebController.java
+++ src/main/java/u1606484/banksim/controllers/WebController.java
@@ -1,5 +1,14 @@
 package u1606484.banksim.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WebController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
 
 }
