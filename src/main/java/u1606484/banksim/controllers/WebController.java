76ae36b42697e3ff33360c1341ee6a6436111a73
angbarn <a.barnes.3@warwick.ac.uk>
2018-03-14 12:35:49 +0000

    Can now attempt to login

--- src/main/java/u1606484/banksim/controllers/WebController.java
+++ src/main/java/u1606484/banksim/controllers/WebController.java
@@ -4,16 +4,46 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import u1606484.banksim.weblogic.LoginSystem;
 
 @RestController
 public class WebController {
+
+    private final LoginSystem loginSystem = new LoginSystem();
 
     @RequestMapping(
             value = "/index",
             method = RequestMethod.GET
     )
-    public Model index(Model model) {
+    public Model index(String loginSuccess, Model model) {
+        model.addAttribute("login_success",
+                loginSuccess.equals("failure")
+                        ? "login_input_failure"
+                        : "login_input");
         return model;
+    }
+
+    @RequestMapping(
+            value = "/attempt_login",
+            method = RequestMethod.POST
+    )
+    public String attemptLoginStage1(String username, String password) {
+        String failRedirect = "redirect:/index?loginSuccess=failure";
+        String successRedirect = "redirect:/attempt_login_2";
+
+        int userId;
+        try {
+            userId = Integer.parseInt(username);
+        } catch (NumberFormatException e) {
+            return failRedirect;
+        }
+
+        boolean loginSuccess = loginSystem.attemptBasicLogin(userId, password);
+        if (loginSuccess) {
+            return successRedirect;
+        } else {
+            return failRedirect;
+        }
     }
 
 }
