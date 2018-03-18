8dd9f56f5036d99a7cd3bcc362a44e4327e51705
angbarn <a.barnes.3@warwick.ac.uk>
2018-03-18 02:40:52 +0000

    Comments for FunctionalHelpers

--- src/main/java/u1606484/banksim/databases/FunctionalHelpers.java
+++ src/main/java/u1606484/banksim/databases/FunctionalHelpers.java
@@ -8,29 +8,67 @@ import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
+/**
+ * Effectively a container to separate a lot of slightly verbose functional
+ * programming code from the rest of the program.
+ *
+ * <p>The contents generally allow for the creation of more generic
+ * "super-methods" to operate on the database.
+ */
 class FunctionalHelpers {
 
-    // Integer -> String -> PreparedStatement
+    /**
+     * Binds a string to a prepared statement
+     *
+     * <p>{@code Integer -> String -> PreparedStatement}
+     */
     private static final Function<Integer, Function<String,
             Consumer<PreparedStatement>>> BIND_STRING = (i) -> (v) ->
             UncheckedConsumer
                     .escapeConsumer((s) -> s.setString(i, v));
-    // Integer -> Integer -> PreparedStatement
+    /**
+     * Binds an integer to a prepared statement
+     *
+     * <p>{@code Integer -> Integer -> PreparedStatement}
+     */
     private static final Function<Integer, Function<Integer,
             Consumer<PreparedStatement>>> BIND_INTEGER = (i) -> (v) ->
             UncheckedConsumer
                     .escapeConsumer((s) -> s.setInt(i, v));
     // Integer -> byte[] -> PreparedStatement
+    /**
+     * Binds a byte array to a prepared statement
+     *
+     * <p>{@code Integer -> byte[] -> PreparedStatement}
+     */
     private static final Function<Integer, Function<byte[],
             Consumer<PreparedStatement>>> BIND_BYTES = (i) -> (v) ->
             UncheckedConsumer
                     .escapeConsumer((s) -> s.setBytes(i, v));
-    // Integer -> Long -> PreparedStatement
+    /**
+     * Binds a long to a prepared statement
+     *
+     * <p>{@code Integer -> Long -> PreparedStatement}
+     */
     private static final Function<Integer, Function<Long,
             Consumer<PreparedStatement>>> BIND_LONG = (i) -> (v) ->
             UncheckedConsumer
                     .escapeConsumer((s) -> s.setLong(i, v));
 
+    /**
+     * Attempts to retrieve a single record of data from a {@link ResultSet}.
+     * This operation accounts for the majority of all reading from the
+     * database.
+     *
+     * <p>A handler function is applied to the first row of the database in
+     * order to control what sort of data this generic method retrieves.
+     *
+     * @param rs The result set to retrieve from
+     * @param handler The handler function to apply to the first row
+     * @param <T> The type of data the returned optional contains
+     * @return An optional containing the returned data, or if no rows were
+     * returned by the query, an empty optional.
+     */
     static <T> Optional<T> attemptSingleRetrieval(ResultSet rs,
             UncheckedFunction<ResultSet, T> handler) {
         try {
@@ -79,6 +117,14 @@ class FunctionalHelpers {
         void accept(T t) throws SQLException;
     }
 
+    /**
+     * A function whose {@link Function#apply(Object)} method is marked as
+     * throwing a {@link SQLException}, allowing lambda functions using SQL
+     * methods to be written.
+     *
+     * @param <T> The type of data the function takes
+     * @param <R> The type of data the function returns
+     */
     @FunctionalInterface
     interface UncheckedFunction<T, R> {
 
@@ -114,8 +160,18 @@ class FunctionalHelpers {
      */
     abstract static class DatabaseBinding<T> {
 
+        /**
+         * Bind position in the prepared statement
+         */
         private final int index;
+        /**
+         * The value to bind to the statement
+         */
         private final T value;
+        /**
+         * The "handler function" to manage the binding of the value to the
+         * prepared statement.
+         */
         private final Function<Integer, Function<T,
                 Consumer<PreparedStatement>>>
                 binder;
@@ -128,11 +184,20 @@ class FunctionalHelpers {
             this.binder = binder;
         }
 
+        /**
+         * Binds the provided value to the query, at provided position using
+         * provided binding function.
+         * @param s The PreparedStatement to bind to
+         */
         void performBinding(PreparedStatement s) {
             binder.apply(index).apply(value).accept(s);
         }
     }
 
+    /**
+     * A version of {@link DatabaseBinding} specified to use only strings,
+     * yielding more consise code
+     */
     static class bString extends DatabaseBinding<String> {
 
         bString(int index, String value) {
@@ -140,6 +205,10 @@ class FunctionalHelpers {
         }
     }
 
+
+    /**
+     * A version of {@link DatabaseBinding} specified to use only integers, yielding more consise code
+     */
     static class bInteger extends DatabaseBinding<Integer> {
 
         bInteger(int index, Integer value) {
@@ -147,6 +216,11 @@ class FunctionalHelpers {
         }
     }
 
+
+    /**
+     * A version of {@link DatabaseBinding} specified to use only byte
+     * arrays, yielding more consise code
+     */
     static class bBytes extends DatabaseBinding<byte[]> {
 
         bBytes(int index, byte[] value) {
@@ -154,6 +228,10 @@ class FunctionalHelpers {
         }
     }
 
+
+    /**
+     * A version of {@link DatabaseBinding} specified to use only longs, yielding more consise code
+     */
     static class bLong extends DatabaseBinding<Long> {
 
         bLong(int index, Long value) {
