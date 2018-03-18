2abbe07772b4da57f0ea9d68dfa5a416d1ce3636
angbarn <a.barnes.3@warwick.ac.uk>
2018-03-18 04:22:54 +0000

    2FA comments

--- src/main/java/u1606484/banksim/TwoFactorService.java
+++ src/main/java/u1606484/banksim/TwoFactorService.java
@@ -8,12 +8,32 @@ import java.security.SecureRandom;
 import u1606484.banksim.interfaces.IOtacGenerator;
 import u1606484.banksim.interfaces.ITwoFactorService;
 
+/**
+ * An implementation of the {@link ITwoFactorService} interface using Twilio's
+ * SMS API.
+ *
+ * <p>The bulk of the work is handlded by Twilio's API, so this class
+ * effectively acts as a wrapper around that API.
+ */
 public class TwoFactorService implements ITwoFactorService {
 
+    /**
+     * The account SID to sign in to Twilio with
+     */
     private final static String ACCOUNT_SID = System
             .getenv("twilio_account_sid");
+    /**
+     * The API key assigned to the account by twilio
+     */
     private final static String AUTH_TOKEN = System.getenv("twilio_auth_token");
+    /**
+     * The OTAC generator to use when providing OTACs.
+     */
     private final IOtacGenerator generator;
+    /**
+     * The maximum acceptable number of windows away from a current OTAC a
+     * provided OTAC may be to still be accepted.
+     */
     private final int otacStepWindowCount;
 
     public TwoFactorService(int otacLength, int otacStepSize,
@@ -54,12 +74,25 @@ public class TwoFactorService implements ITwoFactorService {
         System.out.println(m.getBody());
     }
 
+    /**
+     * Calls upon the generator to provide an OTAC at a certain offset
+     *
+     * @param secretKey The secret key to use for OTAC generation
+     * @param offset The offset in terms of the number of windows
+     * @return A generated OTAC.
+     */
     @Override
     public String generateOtac(byte[] secretKey, int offset) {
         return generator.generateOtac(secretKey,
                 generator.getTimestamp(System.currentTimeMillis(), offset));
     }
 
+    /**
+     * Gets the window count
+     *
+     * @return The window count
+     * @see ITwoFactorService#getWindowCount()
+     */
     @Override
     public int getWindowCount() {
         return otacStepWindowCount;
